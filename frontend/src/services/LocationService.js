715a9ed625d260e5cd90204bf257998a130eb787
David Sosnac <105160225+Sosnac@users.noreply.github.com>
2026-03-28 16:23:46 +0200

    Create frontend/src/services/LocationService.js

---
+++ frontend/src/services/LocationService.js
@@ -0,0 +1,23 @@
+import * as Location from 'expo-location';
+
+export const getCurrentLocation = async () => {
+  try {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      console.warn('Permission to access location was denied');
+      return null;
+    }
+
+    let location = await Location.getCurrentPositionAsync({
+      accuracy: Location.Accuracy.High,
+    });
+
+    return {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+    };
+  } catch (error) {
+    console.error("Location fetch failed", error);
+    return null;
+  }
+};
